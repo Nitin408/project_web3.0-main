06d02f7381511cde76fe350e4230859fe4b9caab
Nitin Baghel <nitinbaghel408@gmail.com>
2022-08-30 16:48:21 +0530

    updated hardhat version

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,13 @@
+//https://eth-mainnet.g.alchemy.com/v2/yz61mZXKFHsCOSOILbR3tcPNkCx9urbT
+
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+      url: 'https://eth-mainnet.g.alchemy.com/v2/yz61mZXKFHsCOSOILbR3tcPNkCx9urbT',
+      accounts: ['90001165fbb6b1cf8ed5e5706a0b6900a808fb74f7de3f84b7b391ba86e899f6'],
     },
   },
 };
