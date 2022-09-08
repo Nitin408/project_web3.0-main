2bfc19524dfd70984c0a2932dbd62faabb3351a5
Nitin Baghel <nitinbaghel408@gmail.com>
2022-09-08 12:21:43 +0530

    scommin\

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,17 @@
-//https://eth-goerli.g.alchemy.com/v2/5U_hwuh8-gHxzFl1YAT3uj8odSYRpJ9l
+// //https://eth-goerli.g.alchemy.com/v2/5U_hwuh8-gHxzFl1YAT3uj8odSYRpJ9l
+
+// require('@nomiclabs/hardhat-waffle');
+
+// module.exports = {
+//   solidity: '0.8.0',
+//   networks: {
+//     ropsten: {
+//       url: 'https://eth-goerli.g.alchemy.com/v2/5U_hwuh8-gHxzFl1YAT3uj8odSYRpJ9l',
+//       accounts: ['90001165fbb6b1cf8ed5e5706a0b6900a808fb74f7de3f84b7b391ba86e899f6'],
+//     },
+//   },
+// };
+
 
 require('@nomiclabs/hardhat-waffle');
 
@@ -6,8 +19,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/5U_hwuh8-gHxzFl1YAT3uj8odSYRpJ9l',
-      accounts: ['90001165fbb6b1cf8ed5e5706a0b6900a808fb74f7de3f84b7b391ba86e899f6'],
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
     },
   },
 };
